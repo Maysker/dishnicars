8d1a457ccdff556cd0ccb75df2c3292c6f750d09
Maysker <a.gazdiev@gmail.com>
2023-12-25 04:06:09 +0100

    Added section "Our works"

--- script.js
+++ script.js
@@ -2,9 +2,4 @@ document.addEventListener('DOMContentLoaded', () => {
     // Your slider initialization code
 });
 
-// JavaScript code to toggle the menu
-nav a {
-    color: white; /* Text color for links */
-    text-decoration: none; /* Remove underline from links */
-    padding: 0 20px; /* Space out the links */
-}
+
